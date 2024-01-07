dcbed5976f39e5f8ed3326662587fa2b48c9aec7
sorzechowski <sebastian.orzechowski@ieo.pl>
2024-01-07 17:37:13 +0100

    Initial commit with .gitignore and initial script

---
+++ Analiza.py
@@ -0,0 +1,7 @@
+import easyocr
+
+#reader = easyocr.Reader(['pl'])
+
+#result = reader.readtext('./Paragony/2019.09.16_24001587120190916599614.jpg.png',detail=0)
+
+#print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
