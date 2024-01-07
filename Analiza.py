5d4795725aabf7c49f3feac4d1766851f6ae5cc5
sorzechowski <sebastian.orzechowski@ieo.pl>
2024-01-07 23:44:02 +0100

    Analiza.py changes. First steps to print out pngs into pyCharm console and it actually works

--- Analiza.py
+++ Analiza.py
@@ -1,6 +1,31 @@
-import easyocr
+import pytesseract
+pytesseract.pytesseract.tesseract_cmd=r"D:\\Program Files (x86)\\Tesseract_OCR\\tesseract.exe"
+from PIL import Image
+import os
 
-#reader = easyocr.Reader(['pl'])
+
+# Path to the directory containing all your png receipts
+directory_path = './Paragony'
+all_pngs = [f for f in os.listdir(directory_path) if f.endswith('.png')]
+
+if(all_pngs):
+# Process only the first png file for testing purposes
+    first_png = all_pngs[0]
+    image_path = os.path.join(directory_path, first_png)
+    receipt_image = Image.open(image_path)
+
+    # Use Tesseract to do OCR on the image
+    text = pytesseract.image_to_string(receipt_image, lang='pol')
+
+    # Output the extracted text
+    print(text)
+else:
+    print("No PNG files found in the directory.")
+
+
+
+#print(text)
+
 
 #result = reader.readtext('./Paragony/2019.09.16_24001587120190916599614.jpg.png',detail=0)
 
